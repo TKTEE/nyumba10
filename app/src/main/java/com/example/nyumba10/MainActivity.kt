f1ce864ec170e4bf6c6e0dc6dd96df8c4dcb30e7
eMobilis <consolatamburu99@gmail.com>
2023-05-31 15:42:53 +0300

    first commit

--- app/src/main/java/com/example/nyumba10/MainActivity.kt
+++ app/src/main/java/com/example/nyumba10/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AlertDialogLayout
 import com.google.firebase.auth.FirebaseAuth
 
@@ -38,6 +39,21 @@ class MainActivity : AppCompatActivity() {
             }else if(password.isEmpty()){
                 edtPassword.setError("Please fill this input")
                 edtPassword.requestFocus()
+            }else{
+                progress.show()
+                mAuth.createUserWithEmailAndPassword(email, password)
+                     .addOnCompleteListener{
+                         progress.dismiss()
+                         if(it.isSuccessful){
+                             Toast.makeText(this,"Registration successful",
+                             Toast.LENGTH_SHORT).show()
+                             mAuth.signOut()
+                             startActivity(Intent(this, LoginActivity::class.java))
+                             finish()
+                         }else{
+                             displayMessage("ERROR", it.exception!!.message.toString())
+                         }
+                     }
             }
         }
         tvLogin.setOnClickListener {
